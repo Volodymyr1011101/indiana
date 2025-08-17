d50ba47b325fe8ccec182cc6349f1df68e0b0fec
Volodymyr Voronkin <volodymyr.voronkin@Volodymyrs-MacBook-Pro.local>
2025-08-17 21:34:35 +0200

    added squid land

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,10 +3,10 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
     // A list of all locales that are supported
-    locales: ['pt'],
+    locales: ['en'],
 
     // Used when no locale matches
-    defaultLocale: 'pt',
+    defaultLocale: 'en',
     localePrefix: 'never',
     localeDetection: true,
 

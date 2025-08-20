edbc46bcc688539bab4dcb58b5e7e84c855ac1e5
Volodymyr Voronkin <volodymyr.voronkin@Volodymyrs-MacBook-Pro.local>
2025-08-20 09:57:55 +0200

    fix translate

--- src/app/[locale]/(pages)/page.tsx
+++ src/app/[locale]/(pages)/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
                     <h2>
                         {t('headerTop')}
                     </h2>
-                    <h1>
+                    <h1 style={{ whiteSpace: 'pre-line' }}>
                         {t('header')}
                     </h1>
                 </div>
